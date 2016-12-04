e169b099381a58dc7714f5b247a51bed85d169da
Richard <Richard@acer_v17>
2016-12-04 18:56:00 +0100

    init

---
+++ math/src/com/tool/rika/Math.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.tool.rika;
+
+/**
+ * @author Richard
+ * 
+ * Start
+ *
+ */
+public class Math {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
